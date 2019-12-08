7e0c18f1a38df71d7f51b5a360267459175cd1f4
Kert Tamm <31847373+K3RTTAMM@users.noreply.github.com>
2019-12-08 22:41:58 +0200

    updated views.py

--- django/django_app/todo/todo_list/views.py
+++ django/django_app/todo/todo_list/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
+from .forms import listForm
+from django.contrib import messages
 
 def home(request):
-    return render(request, "home.html", {})
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, ('Item Has Been Added To List!'))
+            return render(request, 'home.html', {'all_items': all_items})
+
+        else:
+            all_items = List.objects.all
+            return render(request, 'home.html', {'all_items': all_items})
